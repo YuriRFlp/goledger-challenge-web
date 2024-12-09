65c606de26bbc9f56bfdfb3456d51ffb2d3d7409
yurirflp96 <yuri.felipe@bamaqconsorcio.com.br>
2024-12-09 12:06:59 -0300

    feat: create crud artist

---
+++ app/artist/create/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Button from "@/app/components/Button";
+import { Icons } from "@/app/components/Icons";
+import Input from "@/app/components/Input";
+import { GlobalContext } from "@/app/context";
+import { usePathname } from "next/navigation";
+import { useContext } from "react";
+
+export default function ArtistCreate() {
+    const { formData, changeFormData, handleCreateUpdateAsset, loading } = useContext(GlobalContext);
+    const pathname = usePathname();
+
+    return (
+        <main className="w-[100%] h-[100%] px-4 md:w-[95%] md:h-auto md:h-[95%] md:min-h-[95%] py-6 md:px-8 rounded bg-black">
+            <div className="md:flex items-center justify-between mb-10">
+                <h1 className="text-2xl mb-5 md:mb-0">Criar Artista <span className="text-base">- Preencha os campos a seguir para criar um artista.</span></h1>
+                <Button text="Salvar" rounded onClick={() => handleCreateUpdateAsset(pathname.split('/')[1], 'create')}>
+                    <Icons.save className="w-6 h-6 mr-2" />
+                </Button>
+            </div>
+
+            <form className="grid grid-cols-1 gap-2 md:grid-cols-2 md:gap-10">
+                <Input
+                    label="Nome"
+                    type="text"
+                    name="name"
+                    value={formData.name}
+                    changeEvent={changeFormData}
+                    placeholder="Nome"
+                />
+                <Input
+                    label="País"
+                    type="text"
+                    name="country"
+                    value={formData.country}
+                    changeEvent={changeFormData}
+                    placeholder="País"
+                />
+            </form>
+        </main>
+    );
+}
---
+++ app/artist/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useContext } from "react";
+import { GlobalContext } from "../context";
+import DataTable from "../components/DataTable";
+import Button from "../components/Button";
+import { Icons } from "../components/Icons";
+
+export default function Artist() {
+  const { data, navigate, loading } = useContext(GlobalContext);
+
+  return (
+    <>
+      <main className="w-[100%] h-[100%] px-4 md:w-[95%] md:h-auto md:h-[95%] md:min-h-[95%] py-6 md:px-8 rounded bg-black">
+        <div className="md:flex items-center justify-between mb-10">
+          <h1 className="text-2xl mb-5 md:mb-0">Artistas <span className="text-base">- Confira a seguir a lista de artistas disponíveis.</span></h1>
+          <Button text="Adicionar" rounded onClick={() => navigate('/artist/create')}>
+            <Icons.plus className="w-6 h-6 mr-2" />
+          </Button>
+        </div>
+
+        <DataTable data={data} headers={[
+          {name: 'Nome', key: 'name'},
+          {name: 'País', key: 'country'},
+          {name: 'Ações', key: 'actions'},
+        ]} />
+      </main>
+    </>
+  );
+}
