92824ca053100f7b2eebaf9755afdf8084802056
yurirflp96 <yuri.felipe@bamaqconsorcio.com.br>
2024-12-09 12:06:44 -0300

    feat: create crud song

---
+++ app/song/edit/page.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import Button from "@/app/components/Button";
+import { Icons } from "@/app/components/Icons";
+import Input from "@/app/components/Input";
+import Select from "@/app/components/Select";
+import { GlobalContext } from "@/app/context";
+import { usePathname } from "next/navigation";
+import { useContext } from "react";
+
+export default function SongEdit() {
+    const { formData, changeFormData, handleCreateUpdateAsset, albums } = useContext(GlobalContext);
+    const pathname = usePathname();
+
+    const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement | HTMLInputElement>) => {
+        changeFormData(e as React.ChangeEvent<HTMLInputElement>);
+    };
+
+    return (
+        <main className="w-[100%] h-[100%] px-4 md:w-[95%] md:h-auto md:h-[95%] md:min-h-[95%] py-6 md:px-8 rounded bg-black">
+            <div className="md:flex items-center justify-between mb-10">
+                <h1 className="text-2xl mb-5 md:mb-0">Editar Música <span className="text-base">- Edite os campos a seguir para alterar a música.</span></h1>
+                <Button text="Salvar" rounded onClick={() => handleCreateUpdateAsset(pathname.split('/')[1], 'update')}>
+                    <Icons.save className="w-6 h-6 mr-2" />
+                </Button>
+            </div>
+
+            <form className="grid grid-cols-1 gap-2 md:grid-cols-2 md:gap-10">
+                <Input
+                    label="Nome"
+                    type="text"
+                    name="name"
+                    value={formData.name}
+                    changeEvent={changeFormData}
+                    placeholder="Nome"
+                />
+                <Select
+                    label="Álbum"
+                    name="album"
+                    value={formData.album}
+                    changeEvent={handleSelectChange}
+                    options={albums}
+                />
+            </form>
+        </main>
+    );
+}
